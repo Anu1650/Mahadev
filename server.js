1728b571c7243ef587a7f4a892489c23cfe93869
Anu1650 <aniketigade@gmail.com>
2026-05-24 02:13:01 +0530

    rename INDEX.HTML to index.html, add root route

--- server.js
+++ server.js
@@ -180,6 +180,10 @@ app.delete('/api/admin/contact/:id', requireAdmin, async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'admin.html'));
 });
